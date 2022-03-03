124ee56938bb03666c7002ee53913c7fe41d0df0
MilkyC@t <92247270+A-MilkyCat@users.noreply.github.com>
2022-03-03 10:48:02 +0800

    Add files via upload

--- 河內塔.js
+++ 河內塔.js
@@ -1,13 +1,16 @@
 var num,latest_usable_num = 0;
 var stuff = new Array; 
 var stuff_index = new Array;
+var step = 1, is_moving = false;
+var every_step = new Array;
+//  步驟.給.收  為一數字 ex 第一步 1給3 --> 113
 function check_num(){ 
     num = document.getElementById("num").value;
     document.getElementById("num").value = "";
     try{
         if (isNaN(num)==true) throw "It's not a number";
         if (num=="") throw "Null";
-        HI = Number(num);
+        num = Number(num);
         if (num<1) throw "Too small"
         if (num>15) throw "Too big";
     }catch(respond){
@@ -15,6 +18,7 @@ function check_num(){
         return ;
     }
     document.getElementById("show_respond").innerHTML = "";
+    /////////////////////////////////召喚方塊
     for (var i=1;i<=latest_usable_num;i++){
         var brick = document.getElementById(`brick${i}`);
         brick.remove();
@@ -25,7 +29,6 @@ function check_num(){
     for (var i=num;i>=1;i--){
         var brick = document.createElement("div").cloneNode(true);//important!!!
         brick.className = "brick";
-       // brick.className = "brick_animation";
         brick.id = `brick${i}`;
         brick.innerHTML = i;
         box.append(brick);
@@ -40,10 +43,8 @@ function check_num(){
         }
         brick_image.style.width = i*(180/num) +'px';
     }
-    var brick1 = document.getElementById("brick2");
-}
-var t = 0;
-function move_brick(){//onclick buttom
+    step = 1;
+    every_step = new Array;
     for (var i=1;i<=3;i++){
         stuff[i] = new Array;
         if (i==1)
@@ -59,70 +60,31 @@ function move_brick(){//onclick buttom
     stuff_index[1] = num;
     stuff_index[2] = 0;
     stuff_index[3] = 0;
+    pre_move(1,2,3,num);
+    step = 1;
+    console.log(every_step)
+}
+var t = 0;
+function pre_move(f1, f2, f3, n){
+    if (n==1) {
+        every_step[step] = f3 + 10*f1 + 100*step;
+        step ++;
+	}else{
+        pre_move(f1,f3,f2,n-1); 
+        every_step[step] = f3 + 10*f1 + 100*step;
+        step ++;
+        pre_move(f2,f1,f3,n-1);
+	}
+}
+function auto_move(){//onclick buttom
+    if (step != 1){
+        document.getElementById("show_respond").innerText = "初始狀態才可自動";
+        return;
+    }
     t = 1;
     _move(1,2,3,num);
-    console.log(t);
+    console.log(step);
 }
-////////////////////////////////////////////////////////////////
-function move_brick_up(num,index,boxA_index,boxB_index){
-    var start;
-    start = Date.now();
-    var brick = document.getElementById(`brick${index}`);
-    var boxA = document.getElementById(`box${boxA_index}`);
-    var boxB = document.getElementById(`box${boxB_index}`);
-    var timer = window.setInterval(function(){
-        var timePassed = Date.now()-start;
-        if (timePassed>=2000){
-            clearInterval(timer);
-            return ;
-        }
-        move_to_top(timePassed,index);
-    },20);
-}                   /////0-2000
-                    /////0-180
-function move_to_top(timePassed,index){
-    document.getElementById(`brick${index}`).style.bottom = timePassed / (100/9) + 'px';
-    // console.log(brick_move);
-}
-function move_brick_horizontal(num,index,boxA_index,boxB_index){
-    var start = Date.now();
-    var brick = document.getElementById(`brick${index}`);
-    var boxA = document.getElementById(`box${boxA_index}`);
-    var boxB = document.getElementById(`box${boxB_index}`);
-    var timer = window.setInterval(function(){
-        var timePassed = Date.now()-start;
-        if (timePassed>=2000){
-            clearInterval(timer);
-            return ;
-        }
-        move_to_right(timePassed);
-    },20);
-    
-}                   /////0-2000
-                    /////0-200
-function move_to_right(timePassed){
-    brick1.style.left = timePassed/5  + 'px';
-}
-function move_brick_down(num,index,boxA_index,boxB_index){
-    var start = Date.now();
-    var brick = document.getElementById(`brick${index}`);
-    var boxA = document.getElementById(`box${boxA_index}`);
-    var boxB = document.getElementById(`box${boxB_index}`);
-    var timer = window.setInterval(function(){
-        var timePassed = Date.now()-start;
-        if (timePassed>=2000){
-            clearInterval(timer);
-            return ;
-        }
-        //brick1.style.bottom = 200 + "px";
-        move_to_bottom(timePassed);
-    },20);
-}                   /////0-2000
-                    /////0-180
-function move_to_bottom(timePassed){
-    brick1.style.bottom = 180 - timePassed / (100/9) + 'px';
-}
-//////////////////////////////////////////////
 function _move(f1,f2,f3,n){
     if (n==1) {
         var get3 = document.getElementById(`box${f3}`);
@@ -130,9 +92,6 @@ function _move(f1,f2,f3,n){
         var givething = document.getElementById(`brick${stuff[f1][stuff_index[f1]]}`);
         stuff_index[f3] += 1;
         stuff[f3][stuff_index[f3]] = stuff[f1][stuff_index[f1]];
-        // setTimeout(`move_brick_up(${num},${stuff[f1][stuff_index[f1]]},${f1},${f3})`,1000*(t-1)*3);
-        // setTimeout(`move_brick_horizontal(${num},${stuff[f1][stuff_index[f1]]},${f1},${f3})`,1000*(t-1)*3+1000);
-        // setTimeout(`move_brick_down(${num},${stuff[f1][stuff_index[f1]]},${f1},${f3})`,1000*(t-1)*3+2000);
         stuff_index[f1] -= 1;
         setTimeout(function(){
             get3.appendChild(givething);
@@ -140,23 +99,73 @@ function _move(f1,f2,f3,n){
             givething.style.top = 0 + "px";
         },1000*(t-1)+1000);
         t ++;
+        every_step[step] = f3 + 10*f1 + 100*step;
+        step ++;
 	}else{
-		 _move(f1,f3,f2,n-1); 
-         var get3 = document.getElementById(`box${f3}`);
-         var give1 = document.getElementById(`box${f1}`);
-         var givething = document.getElementById(`brick${stuff[f1][stuff_index[f1]]}`);
-         stuff_index[f3] += 1;
-         stuff[f3][stuff_index[f3]] = stuff[f1][stuff_index[f1]];
-        //  setTimeout(`move_brick_up(${num},${stuff[f1][stuff_index[f1]]},${f1},${f3})`,1000*(t-1)*3);
-        //  setTimeout(`move_brick_horizontal(${num},${stuff[f1][stuff_index[f1]]},${f1},${f3})`,1000*(t-1)*3+1000);
-        //  setTimeout(`move_brick_down(${num},${stuff[f1][stuff_index[f1]]},${f1},${f3})`,1000*(t-1)*3+2000);
-         stuff_index[f1] -= 1;
-         setTimeout(function(){
+            _move(f1,f3,f2,n-1); 
+            /////////////
+        // 以下是依給三
+            var get3 = document.getElementById(`box${f3}`);
+            var give1 = document.getElementById(`box${f1}`);
+            var givething = document.getElementById(`brick${stuff[f1][stuff_index[f1]]}`);
+            stuff_index[f3] += 1;
+            stuff[f3][stuff_index[f3]] = stuff[f1][stuff_index[f1]];
+            stuff_index[f1] -= 1;
+            setTimeout(function(){
             get3.appendChild(givething);
             givething.style.left = 0 + "px";
             givething.style.top = 0 + "px";
         },1000*(t-1)+1000);
         t ++;
+        every_step[step] = f3 + 10*f1 + 100*step;
+        step ++;
 		_move(f2,f1,f3,n-1);
 	}
+}
+function Prev(){
+    console.log(every_step)
+    if (is_moving == true){
+        document.getElementById("show_respond2").innerText = "Box is moving";
+        return;
+    }
+    if (step <= 1){
+        return;
+    }else{
+        step = step-1;
+        var givebox = every_step[step] % 10;
+        var give1 = document.getElementById(`box${givebox}`)
+        var buffer = parseInt(every_step[step] / 10);
+        var getbox = buffer % 10;
+        var get = document.getElementById(`box${getbox}`)
+        console.log(givebox, getbox)
+        var givething = document.getElementById(`brick${stuff[givebox][stuff_index[givebox]]}`);
+        stuff_index[getbox] += 1;
+        stuff[getbox][stuff_index[getbox]] = stuff[givebox][stuff_index[givebox]];
+        stuff_index[givebox] -= 1;
+        get.appendChild(givething);
+    }
+}
+function Next(){
+    var respond;
+    if (is_moving == true){
+        respond = document.getElementById("show_respond2")
+        respond.innerText = "Box is moving";
+        return;
+    }
+    if (step >= Math.pow(2, num)){
+        return;
+    }else{
+        var getbox = every_step[step] % 10;
+        var buffer = parseInt(every_step[step] / 10);
+        var givebox = buffer % 10;
+        var give1 = document.getElementById(`box${givebox}`)
+        var get1 = document.getElementById(`box${getbox}`)
+        console.log(givebox, getbox)
+        var givething = document.getElementById(`brick${stuff[givebox][stuff_index[givebox]]}`);
+        stuff_index[getbox] += 1;
+        stuff[getbox][stuff_index[getbox]] = stuff[givebox][stuff_index[givebox]];
+        stuff_index[givebox] -= 1;
+        get1.appendChild(givething);
+        step += 1;
+    }
 }
